c34f7f63621f105ca68da98aa0ece82c11faae1c
Matheus Seabra <matheus@academicmerit.com>
2019-04-20 17:37:09 -0300

    Added database production url

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
-const DB_LOCAL_URL = process.env.DB_LOCAL_URL || "mongodb://localhost:27017/techteams-graphql-db";
+const DB_LOCAL = process.env.DB_LOCAL || "mongodb://localhost:27017/techteams-graphql-db";
+const DB_PROD = process.env.DB_PROD || "mongodb+srv://matheuseabra:abcd12@techteams-graphql-cluster-2hwqi.mongodb.net/test?retryWrites=true";
+mongoose.Promise = global.Promise;
+mongoose.set('useFindAndModify', false);
 
-mongoose.connect(DB_LOCAL_URL, {
+mongoose.connect(DB_PROD, {
   useNewUrlParser: true
 });
 
